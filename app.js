27787c8802789fda3c04433c79a33c7c4d20214e
Binod Chaudhary <thebinod7@gmail.com>
2017-05-21 18:55:45 +0545

    chat app v1.0 complete

--- app.js
+++ app.js
@@ -1,33 +1,68 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+const express = require('express');
 const path = require('path');
 var cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+const server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 users = [];
 connections = [];
+
+//const apiAdmin = require('./routes/admin');
+
+var port=Number(process.env.PORT || 4000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-var port=Number(process.env.PORT || 4141);
 
-app.use(express.static('public'))
+//Body parser middleware
+// parse application/x-www-form-urlencoded.
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// ROUTES FOR OUR API
 app.use('/', require('./routes'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Start server
-app.listen(port,function () {
-    console.log('Server running at port:' + port);
+server.listen(port,function () {
+  console.log('Server running at port:' + port);
 });
 
-io.sockets.on('connection',function (socket) {
-    connections.push(socket);
-    console.log('Connected: %s sockets connected!', connections.length);
+io.sockets.on('connection', function (socket) {
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
 
-    //Disconnect
-    connections.splice(connections.indexOf(socket),1);
-    console.log('Disconnected: %s sockets disconnected!', connections.length);
-})
+  //Disconnect
+  socket.on('disconnect',function (data) {
+   // if(!socket.username) return;
+    users.splice(users.indexOf(socket),1);
+    updateUsername();
+    connections.splice(connections.indexOf(socket),1)
+    console.log('Disconnected: %s sockets disconnected', connections.length);
+  });
+
+  //Send message
+  socket.on('send message',function (data) {
+    console.log(data);
+    io.sockets.emit('new message',{msg:data, user:socket.username});
+  });
+  
+  //New User 
+  socket.on('new user',function (data,callback) {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsername();
+  });
+  
+  function updateUsername() {
+    io.sockets.emit('get users',users);
+  }
+});
